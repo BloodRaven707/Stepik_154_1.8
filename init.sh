d8f0166c00bb810f7013f47bae703467f605309e
Dmitriy Dzhurabaev <blood.raven.707@gmail.com>
2022-10-18 12:34:02 +0300

    add some

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo cp Stepik_154_1.8/init.sh /home/box/
+sudo cp -R Stepik_154_1.8/web/ /home/box/
+sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
